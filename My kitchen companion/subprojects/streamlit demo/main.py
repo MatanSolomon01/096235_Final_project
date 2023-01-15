9a0adfd91040cd1f8e76a5ef5df5516584b86a40
MatanSolomon01 <matansoso7@gmail.com>
2023-01-15 20:47:20 +0200

    Proceed with the demo

--- My kitchen companion/subprojects/streamlit demo/main.py
+++ My kitchen companion/subprojects/streamlit demo/main.py
@@ -1,7 +1,65 @@
 import streamlit as st
-import pandas as pd
 
-st.header("Zdayen Nitay")
-st.write("""
-Hello *world!*
-""")
+import utils
+from utils import helps
+import pandas as pd
+import numpy as np
+import time
+
+st.set_page_config(page_title="My kitchen companion", page_icon=":cookie:", layout="wide")
+
+# Sidebar
+sdb_header = st.sidebar.header("Menu")
+sdb_myzone_btn = st.sidebar.button("My zone", help=helps["sdb_myzone_btn"])
+sdb_tut_btn = st.sidebar.button("Tutorial", help=helps["sdb_tut_btn"])
+sdb_about_btn = st.sidebar.button("About us", help=helps["sdb_about_btn"])
+
+# Header
+st.header(":cookie: My Kitchen Companion")
+st.write("---")
+
+# Panels
+left_panel, right_panel = st.columns(2)
+
+with right_panel:
+    st.write("Choose your flavours!")
+    columns = st.columns(4)
+    with columns[0]:
+        sweet = st.checkbox("Sweet")
+    with columns[1]:
+        sour = st.checkbox("Sour")
+    with columns[2]:
+        salty = st.checkbox("Salty")
+    with columns[3]:
+        bitter = st.checkbox("Bitter")
+
+    st.write('<style>div.row-widget.stRadio div{flex-direction:row;}</style>', unsafe_allow_html=True)
+    st.write('<style>.st-cx{margin-right: auto;}</style>', unsafe_allow_html=True)
+    temperature = st.radio("Choose your temperature!", ['Freeze', 'Cold', 'Natural', 'Hot'], help=helps["temperature"])
+
+    columns = st.columns(2)
+    with columns[0]:
+        include = st.multiselect("Ingredients to include", options=("a",), help=helps["include"])
+    with columns[1]:
+        st.write()
+        exclude = st.multiselect("Ingredients to exclude", options=("a",), help=helps["exclude"])
+
+    preparation_time = st.slider("Preparation time", min_value=0, max_value=100, value=50, step=5, format="",
+                                 help=helps["preparation_time"])
+    healthiness = st.slider("Healthiness", min_value=0, max_value=100, value=50, step=5, format="",
+                            help=helps["healthiness"])
+
+with left_panel:
+    with st.spinner("Loading a recipe...:cookie:"):
+        time.sleep(5)
+        if sweet:
+            name, ingredients, instructions = utils.get_recipe()
+            st.write(f"**{name}**")
+            st.write('')
+            ingredients = ingredients.split('\n')
+            for i in ingredients:
+                st.markdown("- " + i)
+            st.write('')
+            instructions = instructions.split('\n')
+            for i, ins in enumerate(instructions):
+                st.markdown(f"{i+1}. " + ins)
---
+++ My kitchen companion/subprojects/streamlit demo/utils.py
@@ -0,0 +1,28 @@
+helps = {"sdb_myzone_btn": "a",
+         "sdb_tut_btn": "a",
+         "sdb_about_btn": "a",
+         "temperature": "a",
+         "include": "a",
+         "exclude": "a",
+         "preparation_time": "a",
+         "healthiness": "a",
+         "": ""}
+
+
+def get_recipe():
+    name = """Cool 'n Easy Creamy Watermelon Pie"""
+    ingredients = """1 (3 ounce) package watermelon gelatin 
+1⁄4 cup boiling water 
+1 (12 ounce) package Cool Whip, thawed 
+2 cups cubed seedless watermelon 
+1 graham cracker crust"""
+
+    instructions = """Dissolve Jello in boiling water.
+Allow to cool to room temp.
+Whisk in Cool Whip.
+Fold in watermelon.
+Spoon into crust.
+Chill for 2-3 hours or overnight.
+Yum!"""
+
+    return name, ingredients, instructions
